940edf82cecc742700e0e0fb8aa7717ffd67545f
ChristopherSMM <129230277+ChristopherSMM@users.noreply.github.com>
2023-07-27 21:08:38 -0700

    Added index and user java

---
+++ models/user.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+    });
+  
+    User.associate = (models) => {
+      User.hasMany(models.Recipe, {
+        foreignKey: 'userId'
+      });
+      User.hasMany(models.Review, {
+        foreignKey: 'userId'
+      });
+    };
+  
+    return User;
+  };
