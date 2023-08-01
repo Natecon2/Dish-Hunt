20fa3e99925871aa757a899c455595bb51958070
Roberthall653 <roberthall653@gmail.com>
2023-07-31 19:40:24 -0700

    model fixes

---
+++ models/index.js
@@ -0,0 +1,18 @@
+const User = require('./user')
+const Recipe = require('./recipe')
+
+Recipe.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+Recipe.hasMany(models.Review, {
+  foreignKey: 'recipeId'
+});
+
+User.hasMany(User, {
+  foreignKey: 'userId'
+});
+
+module.exports = db;
+
